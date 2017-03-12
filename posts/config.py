785780868763351561fa9fab7a3738896e320ab5
Ben Altieri <chiefspace@gmail.com>
2017-03-12 18:49:57 +0000

    Added TravisConfig class to config.y

--- posts/config.py
+++ posts/config.py
@@ -5,3 +5,8 @@ class DevelopmentConfig(object):
 class TestingConfig(object):
     DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/posts-test"
     DEBUG = True
+    
+class TravisConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu@localhost:5432/posts-test"
+    DEBUG = False
+    SECRET_KEY = "Not secret"
